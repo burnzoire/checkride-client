e0a37406cfdc1e013566818e24be7be86caf83d4
Nathan Burns <burnzoire@gmail.com>
2021-07-09 16:20:44 +1000

    logs kill events

--- DCS-Quoll/Scripts/DCS-QuollGameGUI.lua
+++ DCS-Quoll/Scripts/DCS-QuollGameGUI.lua
@@ -10,12 +10,30 @@ function Quoll.log(str)
     end
 end
 
-Quoll.onGameEvent = function(eventName,arg1,arg2,arg3,arg4)
-    Quoll.log("on game event: "..eventName..", "..arg1..", "..arg2..", "..arg3..", "..arg4)
+Quoll.onNetConnect = function(localPlayerID)
+    local name = net.get_player_info(localPlayerID, "name" )
+    Quoll.log("onNetConnect "..name)
+end
+
+Quoll.onGameEvent = function(eventName,arg1,arg2,arg3,arg4,arg5,arg6,arg7)
+    Quoll.log("on game event: "..eventName..", "..arg1..", "..arg2..", "..arg3..", "..arg4..", "..arg5..", "..arg6..", "..arg7)
+    
+    if eventName == "kill" then
+        local killerPlayerID = arg1
+        local killerName = net.get_player_info(killerPlayerID, "name" )
+        local killerUnitType = arg2
+        local killerSide = arg3
+        local victimPlayerID = arg4
+        local victimUnitType = arg5
+        local victimSide = arg6
+        local weaponName = arg7
+        Quoll.log(killerName.."("..killerUnitType..") destroyed "..victimUnitType.." with "..weaponName)
+    end
 end
 
 Quoll.onChatMessage = function(message, from)
-    Quoll.log("Message: "..from.." - "..message)
+    local name = net.get_player_info(from, "name" )
+    Quoll.log("Message: ["..from.."] "..name.." - "..message)
 end
 
 DCS.setUserCallbacks(Quoll)

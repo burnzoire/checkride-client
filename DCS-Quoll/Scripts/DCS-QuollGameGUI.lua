2cdf58cdf9c88be9ec78ae283f6d5457d2a555b7
Nathan Burns <burnzoire@gmail.com>
2021-07-09 14:42:26 +1000

    adds logging

--- DCS-Quoll/Scripts/DCS-QuollGameGUI.lua
+++ DCS-Quoll/Scripts/DCS-QuollGameGUI.lua
@@ -1,12 +1,21 @@
 net.log("Loading - DCS-Quoll GameGUI")
 
-userCallbacks = {}
-
-userCallbacks.onGameEvent = function(_event)
-    net.log("on game event: ".._event)
+Quoll = {}
+Quoll.dbg = {}
+Quoll.logFile = io.open(lfs.writedir()..[[Logs\DCS-Quoll-GameGUI.log]], "w")
+function Quoll.log(str)
+    if Quoll.logFile then
+        Quoll.logFile:write(str.."\n")
+        Quoll.logFile:flush()
+    end
 end
 
-DCS.setUserCallbacks(userCallbacks)
+Quoll.onGameEvent = function(eventName,arg1,arg2,arg3,arg4)
+    Quoll.log("on game event: "..eventName..", "..arg1..", "..arg2..", "..arg3..", "..arg4)
+end
+
+DCS.setUserCallbacks(Quoll)
 
 net.log("Loaded - DCS-Quoll GameGUI")
+Quoll.log("Quoll loaded")
 

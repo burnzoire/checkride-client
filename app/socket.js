12217c1c0416c9ba46e3b4f9a6c9d559a8840a10
Nathan Burns <burnzoire@gmail.com>
2021-08-03 11:48:53 +1000

    send event to server

--- app/socket.js
+++ app/socket.js
@@ -1,4 +1,6 @@
 import dgram from 'dgram'
+import http from 'http'
+
 import path from 'path'
 import { app, Menu, Tray, BrowserWindow, globalShortcut } from 'electron'
 
@@ -50,18 +52,62 @@ app.on('window-all-closed', function () {
 const server = dgram.createSocket('udp4');
 
 server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
-  server.close();
+  console.log(`server error:\n${err.stack}`)
+  server.close()
 });
 
 server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-});
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
+  let event = JSON.parse(msg)
+  if(event.type == "kill") {
+    console.log(`killer: ${event.killerName}, killer unit: ${event.killerUnitType}, victim: ${event.victimName}, victim unit: ${event.victimUnitType}, weapon: ${event.weaponName}`)
+  }
+
+  const payload = new TextEncoder().encode(
+    JSON.stringify({
+      kill_event: {
+        killer_name: event.killerName,
+        killer_unit_name: event.killerUnitType,
+        victim_name: event.victimName,
+        victim_unit_name: event.victimUnitType,
+        weapon_name: event.weaponName
+      }
+    })
+  )
+
+  var options = {
+    host: 'localhost',
+    path: '/kill_events',
+    port: '3000',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': payload.length
+    }
+  }
+  
+  let req = http.request(options, (response) => {
+    let str = ''
+    response.on('data', (chunk) => {
+      str += chunk
+    })
+  
+    response.on('end', () => {
+      console.log(str)
+    })
+
+    response.on('error', error => {
+      console.error(error)
+    })
+  })
+  req.write(payload)
+  req.end()
+})
 
 server.on('listening', () => {
-  const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
-});
+  const address = server.address()
+  console.log(`server listening ${address.address}:${address.port}`)
+})
 
-server.bind(41234);
+server.bind(41234)
 // Prints: server listening 0.0.0.0:41234

7ae10764e833d19868fcbc6b4afd9997d2ec7330
Nathan Burns <nburns@zendesk.com>
2021-08-03 10:41:51 +1000

    adds system tray

--- app/socket.js
+++ app/socket.js
@@ -1,4 +1,51 @@
-import dgram from 'dgram';
+import dgram from 'dgram'
+import path from 'path'
+import { app, Menu, Tray, BrowserWindow, globalShortcut } from 'electron'
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+let tray = null
+app.whenReady().then(() => {
+  if (app.dock) { app.dock.hide() }
+  // createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+  tray = new Tray('./assets/icon.png')
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'About Quoll',
+      role: 'about',
+    },
+    { type: 'separator' },
+    {
+      label: 'Quit Quoll',
+      role: 'quit',
+      accelerator:'CommandOrControl+Q',
+    },
+  ])
+  tray.setToolTip('Quoll')
+  tray.setContextMenu(contextMenu)
+
+  globalShortcut.register('CommandOrControl+Q', () => {
+    app.quit()
+  })
+
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
 
 const server = dgram.createSocket('udp4');
 

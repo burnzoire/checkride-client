4d7f0aaa4d79c3278ece584478e797ac1a4a5a93
Nathan Burns <burnzoire@gmail.com>
2023-08-04 16:15:23 +1000

    add killer and victim side

--- app/events/killEvent.js
+++ app/events/killEvent.js
@@ -7,10 +7,12 @@ class KillEvent extends GameEvent {
     this.killerName = rawEvent.killerName;
     this.killerUnitType = rawEvent.killerUnitType;
     this.killerUnitCategory = rawEvent.killerUnitCategory;
+    this.killerSide = rawEvent.killerSide;
     this.victimUcid = rawEvent.victimUcid;
     this.victimName = rawEvent.victimName;
     this.victimUnitType = rawEvent.victimUnitType;
     this.victimUnitCategory = rawEvent.victimUnitCategory;
+    this.victimSide = rawEvent.victimSide;
     this.weaponName = rawEvent.weaponName;
   }
 
@@ -22,10 +24,12 @@ class KillEvent extends GameEvent {
         killer_name: this.killerName,
         killer_unit_name: this.killerUnitType,
         killer_unit_category: this.killerUnitCategory,
+        killer_size: this.killerSide,
         victim_ucid: this.victimUcid,
         victim_name: this.victimName,
         victim_unit_name: this.victimUnitType,
         victim_unit_category: this.victimUnitCategory,
+        victim_side: this.victimSide,
         weapon_name: this.weaponName
       }
     };
